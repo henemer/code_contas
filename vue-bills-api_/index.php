6261092053c53b7cd945d6ec4c6f836c9a9e1088
Emerson Henning <emerson@henning.com.br>
2016-08-30 18:19:05 +0000

    Incluindo a API REST

---
+++ vue-bills-api_/index.php
@@ -0,0 +1,173 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+$app = new Silex\Application();
+
+function getBills()
+{
+    $json = file_get_contents(__DIR__ . '/bills.json');
+    $data = json_decode($json, true);
+    return $data['bills'];
+}
+
+function getBillsR()
+{
+    $json = file_get_contents(__DIR__ . '/billsr.json');
+    $data = json_decode($json, true);
+    return $data['bills'];
+}
+
+function findIndexById($id)
+{
+    $bills = getBills();
+    foreach ($bills as $key => $bill) {
+        if ($bill['id'] == $id) {
+            return $key;
+        }
+    }
+    return false;
+}
+
+function findIndexByIdR($id)
+{
+    $bills = getBillsR();
+    foreach ($bills as $key => $bill) {
+        if ($bill['id'] == $id) {
+            return $key;
+        }
+    }
+    return false;
+}
+
+function writeBills($bills)
+{
+    $data = ['bills' => $bills];
+    $json = json_encode($data);
+    file_put_contents(__DIR__ . '/bills.json', $json);
+}
+
+function writeBillsR($bills)
+{
+    $data = ['bills' => $bills];
+    $json = json_encode($data);
+    file_put_contents(__DIR__ . '/billsr.json', $json);
+}
+
+
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
+
+$app->get('api/bills', function () use ($app) {
+    $bills = getBills();
+    return $app->json($bills);
+});
+
+$app->get('api/billsr', function () use ($app) {
+    $bills = getBillsR();
+    return $app->json($bills);
+});
+
+
+$app->get('api/bills/total', function () use ($app) {
+    $bills = getBills();
+    $sum=0;
+    foreach ($bills as $value) {
+        $sum += (float)$value['value'];
+    }
+    return $app->json(['total' => $sum]);
+});
+
+$app->get('api/billsr/total', function () use ($app) {
+    $bills = getBillsR();
+    $sum=0;
+    foreach ($bills as $value) {
+        $sum += (float)$value['value'];
+    }
+    return $app->json(['total' => $sum]);
+});
+
+$app->get('api/bills/{id}', function ($id) use ($app) {
+    $bills = getBills();
+    $bill = $bills[findIndexById($id)];
+    return $app->json($bill);
+});
+
+$app->get('api/billsr/{id}', function ($id) use ($app) {
+    $bills = getBillsR();
+    $bill = $bills[findIndexById($id)];
+    return $app->json($bill);
+});
+
+$app->post('api/bills', function (Request $request) use ($app) {
+    $bills = getBills();
+    $data = $request->request->all();
+    $data['id'] = rand(100,100000);
+    $bills[] = $data;
+    writeBills($bills);
+    return $app->json($data);
+});
+
+$app->post('api/billsr', function (Request $request) use ($app) {
+    $bills = getBillsR();
+    $data = $request->request->all();
+    $data['id'] = rand(100,100000);
+    $bills[] = $data;
+    writeBillsR($bills);
+    return $app->json($data);
+});
+
+$app->put('api/bills/{id}', function (Request $request, $id) use ($app) {
+    $bills = getBills();
+    $data = $request->request->all();
+    $index = findIndexById($id);
+    $bills[$index] = $data;
+    $bills[$index]['id'] = (int)$id;
+    writeBills($bills);
+    return $app->json($bills[$index]);
+});
+
+$app->put('api/billsr/{id}', function (Request $request, $id) use ($app) {
+    $bills = getBillsR();
+    $data = $request->request->all();
+    $index = findIndexById($id);
+    $bills[$index] = $data;
+    $bills[$index]['id'] = (int)$id;
+    writeBillsR($bills);
+    return $app->json($bills[$index]);
+});
+
+$app->delete('api/bills/{id}', function ($id) {
+    $bills = getBills();
+    $index = findIndexById($id);
+    array_splice($bills,$index,1);
+    writeBills($bills);
+    return new Response("", 204);
+});
+
+$app->delete('api/billsr/{id}', function ($id) {
+    $bills = getBillsR();
+    $index = findIndexById($id);
+    array_splice($bills,$index,1);
+    writeBillsR($bills);
+    return new Response("", 204);
+});
+
+$app->match("{uri}", function($uri){
+    return "OK";
+})
+->assert('uri', '.*')
+->method("OPTIONS");
+
+
+$app->run();
